a757f72faf474be7b51e8a37ca33fbff9dd11616
Benjamin Wright <benjamin.wright@endurance.com>
2019-03-25 16:45:42 -0400

    updates styles and local state for volume control

--- src/ui/components/interface/ControlBar.jsx
+++ src/ui/components/interface/ControlBar.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 const { ipcRenderer } = require('electron');
+import { PlayerIcon } from 'react-player-controls'
 import styles from '../../styles/components/ControlBar.module';
 
 const ControlBar = (props) => {
 
     useEffect(() => {
-        ipcRenderer.on('volume-status', updateVolume)
+        ipcRenderer.on('volume-status', updateVolume);
+
+        ipcRenderer.send('volume', 'get')
 
         return () => {
             ipcRenderer.removeListener('volume-status', updateVolume)
         };
     }, []);
 
+    const [volumeState, setVolumeState] = useState(true);
+
     const updateVolume = function(event, arg) {
-        console.log(event, arg)
+        setVolumeState(arg);
     }
 
     const changeVolume = function(e, direction) {
@@ -21,15 +26,19 @@ const ControlBar = (props) => {
             e.preventDefault();
         }
 
-        console.log("volume ", direction);
         ipcRenderer.send('volume', direction)
     }
 
     return (
         <div className={styles.controlBar}>
-        <span className={styles.bold}>Volume</span> <a onClick={(e) => {changeVolume(e, "up")}}>Up</a> |
-        <a onClick={(e) => {changeVolume(e, "down")}}>Down</a> |
-        <a onClick={(e) => {changeVolume(e, "mute")}}>Mute</a>
+            <a onClick={(e) => {changeVolume(e, "up")}}>+</a> <span>{ volumeState.volume }</span>
+            <a onClick={(e) => {changeVolume(e, "mute")}} className={styles.iconButton}>
+                {
+                    (volumeState.muted) ?
+                        (<PlayerIcon.SoundOff width={30} height={30} fill="#ff0000" stroke="#ff0000" />) : (<PlayerIcon.SoundOn width={30} height={30} fill="#fff" stroke="#fff" />)
+                }
+            </a>
+            <a onClick={(e) => {changeVolume(e, "down")}}>-</a>
       </div>
     );
 }
